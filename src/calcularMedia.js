55162c8f9a4657470a850d090d6b4f50da5379f6
Lohhan <lohhang05@gmail.com>
2024-11-11 23:08:00 -0300

    atualização do modulo 2

--- src/calcularMedia.js
+++ src/calcularMedia.js
@@ -1,4 +1,4 @@
-const lancarNota = require("./lancamentoNotas");
+const lancarNota = require("./LancamentoNotas");
 
 function calcularMedia(nota1, nota2) {
    const {nota1: notaValidada1, nota2: notaValidada2} = lancarNota(nota1, nota2)

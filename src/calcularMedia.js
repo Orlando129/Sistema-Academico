98713484e7ad8b6c611845e2b695daef9e49579b
Lohhan <lohhang05@gmail.com>
2024-11-12 12:44:27 -0300

    adicionado funcao exibirStatus em calcularMedia

--- src/calcularMedia.js
+++ src/calcularMedia.js
@@ -1,8 +1,10 @@
 const lancarNota = require("./LancamentoNotas");
+const exibirStatus = require("./exibirStatus");
 
 function calcularMedia(nota1, nota2) {
    const {nota1: notaValidada1, nota2: notaValidada2} = lancarNota(nota1, nota2)
    const media = (notaValidada1 + notaValidada2) / 2
+   exibirStatus(media)
 
    return media
 }

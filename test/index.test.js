29256b169fad91c24415b02656107f23c571a88b
Orlando129 <orlandotelles129@gmail.com>
2024-11-11 22:03:20 -0300

    ultimo teste do module 1

--- test/index.test.js
+++ test/index.test.js
@@ -24,4 +24,8 @@ describe("Lançamento de Notas", () => {
       expect(() => lancarNota("sete", 7.3)).toThrow("A nota de 1UP deve ser um número entre 0.0 e 10.0 com uma casa decimal."
         );
       });
+      test('Deve lançar um erro se a entrada de 2UP não for um número', () => {
+        expect(() => lancarNota( 7.5, "sete")).toThrow("A nota de 2UP deve ser um número entre 0.0 e 10.0 com uma casa decimal."
+          );
+        });
   });

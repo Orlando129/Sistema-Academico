48a7d63f2009674844c1fe5988b36156c7149eac
Orlando129 <orlandotelles129@gmail.com>
2024-11-11 21:49:31 -0300

    implementacao dos testes 1

--- src/LancamentoNotas.js
+++ src/LancamentoNotas.js
@@ -1,6 +1,7 @@
 function lancarNota(nota1, nota2){
     const validarNota = (nota) =>{
-        return typeof nota === 'number' && nota >= 0.0 && nota <= 10.0 && /^\d+(\.\d)?$/.test(nota.toFixed(1));
+        const regex = /^\d+(\.\d{1})?$/; // Aceita no máximo uma casa decimal
+        return typeof nota === 'number' && !isNaN(nota) && nota >= 0.0 && nota <= 10.0 && regex.test(nota.toString());
     };
     
     if (!validarNota(nota1)) {
@@ -10,6 +11,6 @@ function lancarNota(nota1, nota2){
         throw new Error("A nota de 2UP deve ser um número entre 0.0 e 10.0 com uma casa decimal.");
     }
 
-    return {nota1: nota1.toFixed(1), nota2: nota2.toFixed(1)};
+    return {nota1, nota2};
 }
 module.exports = lancarNota;
--- test/index.test.js
+++ test/index.test.js
@@ -2,15 +2,26 @@ const lancarNota = require('../src/LancamentoNotas')
  
 describe("Lançamento de Notas", () => {
     test("Deve retornar as notas de 1UP e 2UP para valores válidos", () => {
-      const resultado = lancarNota(8.5, 7.3);
-      expect(resultado).toEqual({ nota1: "8.5", nota2: "7.3" });
+      const resultado = lancarNota(7.5, 8.2);
+      expect(resultado).toEqual({ nota1: 7.5, nota2: 8.2 });
     });
   
     test("Deve lançar erro se a nota de 1UP não for um número válido", () => {
-      expect(() => lancarNota(11.0, 8.5)).toThrow("A nota de 1UP deve ser um número entre 0.0 e 10.0 com uma casa decimal.");
+      expect(() => lancarNota(-1.0, 8.2)).toThrow("A nota de 1UP deve ser um número entre 0.0 e 10.0 com uma casa decimal.");
     });
   
     test("Deve lançar erro se a nota de 2UP não for um número válido", () => {
-      expect(() => lancarNota(8.5, -1.0)).toThrow("A nota de 2UP deve ser um número entre 0.0 e 10.0 com uma casa decimal.");
+      expect(() => lancarNota(7.5, 12.0)).toThrow("A nota de 2UP deve ser um número entre 0.0 e 10.0 com uma casa decimal.");
     });
+
+    test("Nota de 1UP com mais de uma casa decimal", () => {
+      expect(() => lancarNota(7.567, 8.2)).toThrow("A nota de 1UP deve ser um número entre 0.0 e 10.0 com uma casa decimal.");
+      });
+    test("Nota de 2UP com mais de uma casa decimal", () => {
+      expect(() => lancarNota(7.5, 8.256)).toThrow("A nota de 2UP deve ser um número entre 0.0 e 10.0 com uma casa decimal.");
+      });
+    test('Deve lançar um erro se a entrada de 1UP não for um número', () => {
+      expect(() => lancarNota("sete", 7.3)).toThrow("A nota de 1UP deve ser um número entre 0.0 e 10.0 com uma casa decimal."
+        );
+      });
   });
